f3007511b7fdb25c2b6a9585095cc0a92d95a5bb
AlexDominguez <alexdominguezwebdev@gmail.com>
2018-09-23 03:51:17 -0700

    Created JS,CSS, html and md files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function()) {
+  for (var x=1; x <= 100; x++){
+      if( x % 3 && x % 5 ) {
+          document.write(x);
+      } else {
+          if( x % 3 == 0 ) {
+              document.write("ping");
+          }
+          if( x % 5 == 0 ) {
+              document.write("pong");
+          }
+      }
+      document.write('<br>');
+  }
+});
