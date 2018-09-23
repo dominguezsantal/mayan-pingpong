95c5d18b656c93d38c121e3d47541c7809164d0f
AlexDominguez <alexdominguezwebdev@gmail.com>
2018-09-23 06:09:16 -0700

    added for loop for the user to enter a number and count up his/her number from 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-/*for (var x=1; x <= 100; x++){
-      if( x % 3 && x % 5 ) {
-          document.write(x);
-      } else {
-          if( x % 3 == 0 ) {
-              document.write("ping");
-          }
-          if( x % 5 == 0 ) {
-              document.write("pong");
-          }
-      }
-      document.write('<br>');
-  }
+$(document).ready(function(){
+  $("form#numberInput").submit(function() {
+    event.preventDefault();
+    var number = parseInt($("#number").val());
+    var result;
+    for (var x=1; x <= number; x++){
+     if( x % 3 && x % 5 ) {
+         document.write(x);
+     } else {
+         if( x % 3 == 0 ) {
+             document.write("ping");
+         }
+         if( x % 5 == 0 ) {
+             document.write("pong");
+         }
+     }
+     document.write('<br>'); //line breaks to enhance output readability
+   }
+
+   $("#output").text(result);
+  });
+});
