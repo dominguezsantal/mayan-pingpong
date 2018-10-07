2b59ac7fe6667e093957aa1b953ee61278eab2d2
AlexDominguez <alexdominguezwebdev@gmail.com>
2018-10-07 16:37:20 -0700

    added business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
+//Business Logic
+var number = x;
+for (var x = 1; x <= 100; x++) {
+    var output = '';
+    if (x % 3 === 0) output += ('ping');
+    if (x % 5 === 0) output += ('pong');
+    console.log(output || x);
+}
+
+
+
+//User Interface
 $(document).ready(function(){
-  $("form#numberInput").submit(function() {
+  $("form#numberInput").submit(function(event) {
     event.preventDefault();
 
     var number = parseInt($("#number").val());
-    var result;
-    for (var x=1; x <= number; x++){
-     if( x % 3 && x % 5 ) {
-         document.write(x);
-     } else {
-         if( x % 3 == 0 ) {
-             document.write("ping");
-         }
-         if( x % 5 == 0 ) {
-             document.write("pong");
-         }
-         if( x % 15 == 0 ) {
-             document.write("pingpong");
-         }
-     }
-     document.write('<br>'); //line breaks to enhance output readability
-   }
 
-   $("#output").text(result);
+   $("#output").text(output);
 
      });
   });
